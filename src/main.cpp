9613d71e22d93710fe6e420a31d9f7ee82118396
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2026-07-21 15:54:06 +1000

    Refactor the operation of the TFT display into Display class and call from main.cpp to show use.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+// #include <cstdio>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/pio.h"
@@ -6,33 +7,18 @@
 #include "WS2812.pio.h" // This header file gets produced during compilation from the WS2812.pio file
 #include "drivers/logging/logging.h"
 
-#define LED_PIN 14
+#include "peripherals/tft_display.h"
 
 int main()
 {
     stdio_init_all();
 
-    // Initialise PIO0 to control the LED chain
-    uint pio_program_offset = pio_add_program(pio0, &ws2812_program);
-    ws2812_program_init(pio0, 0, pio_program_offset, LED_PIN, 800000, false);
-    uint32_t led_data [1];
+    Display::init();
+    Display::write_text("CAN YOU SEE ME?");
 
     for (;;) {
-        // Test the log system
-        log(LogLevel::INFORMATION, "Hello world");
-
-        // Turn on the first LED to be a certain colour
-        uint8_t red = 0;
-        uint8_t green = 0;
-        uint8_t blue = 255;
-        led_data[0] = (red << 24) | (green << 16) | (blue << 8);
-        pio_sm_put_blocking(pio0, 0, led_data[0]);
-        sleep_ms(500);
-
-        // Set the first LED off 
-        led_data[0] = 0;
-        pio_sm_put_blocking(pio0, 0, led_data[0]);
-        sleep_ms(500);
+        Display::run();
+        sleep_ms(5);
     }
 
     return 0;
---
+++ src/peripherals/tft_display.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Display{
+    public:
+        static void init();
+        static void write_text(const char *string);
+        static void run();
+};
